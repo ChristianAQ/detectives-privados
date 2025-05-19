f4e6292a46ddf172d6836db61d0f7a6b32c1a673
Christian Armijos Quezada <christian.desd@outlook.es>
2025-05-19 16:28:26 +0200

    inicio de proyecto

---
+++ detectives/detectives/src/main/java/com/detectives/detectives/DetectivesApplication.java
@@ -0,0 +1,13 @@
+package com.detectives.detectives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectivesApplication.class, args);
+	}
+
+}
---
+++ detectives/detectives/src/test/java/com/detectives/detectives/DetectivesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.detectives.detectives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectivesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
